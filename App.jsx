ea347fe210356cf496c28a2210adba0a8f680bfb
Anjali Behara <anjalibehara03@gmail.com>
2026-07-15 08:23:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import './App.css';
+const App = () => {
+  return (
+    <div>
+      
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
+
+
